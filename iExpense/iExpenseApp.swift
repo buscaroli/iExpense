4ee944be7f383428219d6247b5c1b63e55210fd1
mabus <m.buscaroli@outlook.com>
2021-05-25 11:09:09 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Matteo on 25/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
